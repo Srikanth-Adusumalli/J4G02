0d33bcd23948438f69351c0818560ad82abe2777
Rahul Rajput <101698795+Rahul170699@users.noreply.github.com>
2022-03-21 12:36:03 +0530

    Add files via upload

---
+++ Rahul Rajput/Assignment-01/Question-06/ArrayDemo.java
@@ -0,0 +1,18 @@
+package com.Question1;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4, 5 };
+		int sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i];
+
+		}
+		System.out.println("sum of elements of array=" + sum);
+
+		int b = sum / a.length;
+		System.out.println("average of array=" + b);
+	}
+}
