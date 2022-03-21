330851e3fa8090222ca1eff2895aa3f77f8afacb
Rahul Rajput <101698795+Rahul170699@users.noreply.github.com>
2022-03-21 21:28:07 +0530

    Add files via upload

---
+++ Rahul Rajput/Assignment-02/Question-03/StringConvert.java
@@ -0,0 +1,14 @@
+package com.Assignment2;
+
+public class StringConvert {
+
+	public static void main(String[] args) {
+		String s = "amit prajapati";
+		System.out.println(s.toUpperCase());
+
+		String s1 = "MOHIT";
+		System.out.println(s1.toLowerCase());
+
+	}
+
+}
