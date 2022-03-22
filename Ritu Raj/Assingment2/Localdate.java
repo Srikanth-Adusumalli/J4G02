1e2899173b331ca2b00afa205adc57ea0460bf04
rituraj121 <98937190+rituraj121@users.noreply.github.com>
2022-03-22 15:09:16 +0530

    Add files via upload

---
+++ Ritu Raj/Assingment2/Localdate.java
@@ -0,0 +1,11 @@
+package Assingment2;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Localdate {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate date = LocalDate.of(1998, Month.MAY, 10);
+		System.out.println("Employee birthday is on: " + date);
+	}
+}
