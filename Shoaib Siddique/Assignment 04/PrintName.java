6fd774b53b2c8dc6997bff0d758d34cd1523863e
Shoaib Siddiqui <62845135+Shoaib-Siddiqui-Github@users.noreply.github.com>
2022-03-22 19:27:53 +0530

    Add files via upload

---
+++ Shoaib Siddique/Assignment 04/PrintName.java
@@ -0,0 +1,31 @@
+
+public class PrintName {
+	static void myMethod() {
+		System.out.println("Shoaib Siddiqui");
+	}
+
+	public static void main(String[] args) {
+		myMethod();
+	}
+
+}
+
+/*2nd Method
+ * public class Task { static String Name = "Shoaib";
+ * 
+ * public static void main(String[] args) {
+ * 
+ * // we can also use System.out.println(Task.Name); }
+ * 
+ * }
+ * 
+ *  3rd Method
+ * public class Task { static String Name = "Shoaib"; 
+ * public static void main(String[] args) { 
+ * System.out.println(Name); }
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
