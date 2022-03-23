b10b6ee428f124aeb88987fee7b66551b2bb84eb
rituraj121 <98937190+rituraj121@users.noreply.github.com>
2022-03-23 21:35:55 +0530

    Delete Ritu Raj/Assingment1 directory

--- Ritu Raj/Assingment1/Book.java
+++
@@ -1,9 +0,0 @@
-package Printname;
-
-public class Book {
-	public void printBook(int bookId, String bookName, String BookAuthor) {
-
-		System.out.println("[bookId=" + bookId + ", bookName=" + bookName + ", BookAuthor=" + BookAuthor + "]");
-
-}
-}
