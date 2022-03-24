3e5dd22947f0153ac10b48856099f8b7b3ea2b80
Rahul Rajput <101698795+Rahul170699@users.noreply.github.com>
2022-03-24 22:44:06 +0530

    Add files via upload

---
+++ Rahul Rajput/Assignment-05/Question-01/ThreadDemo.java
@@ -0,0 +1,8 @@
+package com.Assignment5;
+
+public class ThreadDemo extends Thread {
+	public void run() {
+		System.out.println("HelloWorld");
+	}
+
+}
---
+++ Rahul Rajput/Assignment-05/Question-01/ThreadDemoMain.java
@@ -0,0 +1,11 @@
+package com.Assignment5;
+
+public class ThreadDemoMain {
+
+	public static void main(String[] args) {
+		ThreadDemo td = new ThreadDemo();
+		td.start();
+
+	}
+
+}
