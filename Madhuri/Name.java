21c74008db8e6316d7eb010086b5e740be82181f
Madhuri <101717105+7601061993@users.noreply.github.com>
2022-03-28 12:52:54 +0530

    Add files via upload

---
+++ Madhuri/Name.java
@@ -0,0 +1,10 @@
+package name;
+public class Name {
+	public static String getname() {
+	 return "madhuri";
+	}
+	public static void main(String args[]) {
+	   String name=getname();
+	   System. out.println(name);
+	}    
+}
