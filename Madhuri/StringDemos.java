bbedf552c529de77bd6fbeeb79322b39bb96b90b
Madhuri <101717105+7601061993@users.noreply.github.com>
2022-03-28 16:05:09 +0530

    Add files via upload

---
+++ Madhuri/StringDemos.java
@@ -0,0 +1,12 @@
+package strings;
+public class StringDemos {
+	public static  void main(String[] args) {
+		String course="java";
+		course="Advance java";
+		System.out.println(course);
+		System.out.println(course.charAt(3));
+		course=course.toUpperCase();
+		System.out.println(course);
+	}
+
+}
