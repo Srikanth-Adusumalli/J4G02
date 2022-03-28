0d50d728565018c1e2eb0b17a21aedb354b56b1c
Shoaib Siddiqui <62845135+Shoaib-Siddiqui-Github@users.noreply.github.com>
2022-03-28 22:28:16 +0530

    Add files via upload

---
+++ Shoaib Siddique/Assignment 06/Assignment.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.CallableStatement;
+
+public class Assignment {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		
+	    Class.forName("com.mysql.jdbc.Driver"); 
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost/shoaib","root","123456");
+		System.out.println("Driver is Loaded to work");
+
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery("Select * from product");
+		
+		while(rs.next()){
+			
+			 String productid = rs.getString("productid"); 
+			 String productname = rs.getString("productname"); 
+			 String productprice = rs.getString("productprice");
+			
+		String productcategory = rs.getString("productcategory");
+		 System.out.println(productid+" | "+productname+" | "+productprice+" | "+productcategory);
+		
+		}
+		con.close();
+		st.close();
+		
+		
+	}
+
+}
+
