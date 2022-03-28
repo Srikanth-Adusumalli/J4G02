e01b3c84479bd10a0b386446d5345bfc6bb2207c
Madhuri <101717105+7601061993@users.noreply.github.com>
2022-03-28 14:08:38 +0530

    Add files via upload

---
+++ Madhuri/Bankaccount.java
@@ -0,0 +1,29 @@
+package bankaccount;
+public class Bankaccount {
+	long accountid;
+	String accountname;
+	float accountbalance;
+	float bankbalance;
+	public Bankaccount(long accountid, String accountname, float accountbalance) {
+		super();
+		this.accountid = accountid;
+		this.accountname = accountname;
+		][lm this.accountbalance = accountbalance;
+		
+	}
+	
+public void  deposit(float accountbalance) {
+	this.accountbalance=this.accountbalance+accountbalance;
+
+}
+public void withdraw(float accountbalance) {
+	this.accountbalance=this.accountbalance-accountbalance;
+}
+public static void main(String args[]){
+	Bankaccount b1=new Bankaccount(12345,"madhuri",10500);
+	Bankaccount b2=new Bankaccount(760106,"madhu",17600);
+	b1.deposit(500);
+	b2.withdraw(900);
+System.out.println(b1.accountid+" "+ b1.accountname+""+b1.accountbalance);
+}
+}
