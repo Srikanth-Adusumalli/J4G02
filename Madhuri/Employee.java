7d101b8712daeb443e81051e8e5ca0dbe1f9bf0a
Madhuri <101717105+7601061993@users.noreply.github.com>
2022-03-28 16:24:13 +0530

    Add files via upload

---
+++ Madhuri/Employee.java
@@ -0,0 +1,17 @@
+
+package date;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+public class Employee {
+	public static void main(String[] args) {
+		LocalDate EmployeeBirthday=LocalDate.of(1990,Month.APRIL,6);
+		System.out.println("employeeBirthday");
+		LocalDate nowDate=LocalDate.now();
+		System.out.println(nowDate);
+		System.out.println(Period.between(EmployeeBirthday,nowDate));
+		System.out.println("Employeeage"+ChronoUnit.YEARS.between(EmployeeBirthday, nowDate));
+	}
+
+}
