16b2328b9a21d46eec2244812b601a553e434060
bickesh <101691049+bickesh@users.noreply.github.com>
2022-03-30 19:37:28 +0530

    Add files via upload

--- Bikesh kumar sahoo/assignment-06/JdbcProduct.java
+++ Bikesh kumar sahoo/assignment-06/JdbcProduct.java
@@ -17,15 +17,14 @@ public class JdbcProduct {
 			Statement st = con.createStatement();
 			
 			
-		
-			ResultSet rs1 = st.executeQuery("Select ProductName from products where ProductCatagory= 'garments' ");
-		     
+		ResultSet rs = st.executeQuery("Select * from products");
 			
 			
-			
-			while(rs1.next()) {
-				System.out.println(rs1.getString(1));
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4));
 			}
+			
+		
 
 			
 		} catch (ClassNotFoundException e) {
