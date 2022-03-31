377911b298d5d3cf14055bc7efec85f2531ef37a
rituraj121 <98937190+rituraj121@users.noreply.github.com>
2022-03-31 21:00:36 +0530

    Add files via upload

---
+++ Ritu Raj/Assingment4/Bookobject.java
@@ -0,0 +1,20 @@
+package Assignment4;
+
+import java.util.HashSet;
+
+public class Bookobject {
+
+	public static void main(String[] args) {
+		HashSet<String> hset = new HashSet<String>();
+		hset.add("physics1");
+		hset.add("Indianpolity");
+		hset.add("Economics");
+		hset.add(null);
+		
+		System.out.println(hset);
+
+
+
+	}
+
+}
---
+++ Ritu Raj/Assingment4/Hashmap.java
@@ -0,0 +1,22 @@
+package Assignment4;
+
+public class Hashmap {
+
+	public static void main(String[] args) {
+		java.util.HashMap<Integer, String> map = new java.util.HashMap<Integer, String>();
+		map.put(1, "ritu");
+		map.put(2, "shetty");
+		map.put(3, "sunny");
+		map.put(4, "anil");
+		map.put(6, "sharanu");
+		map.put(7, "kedar");
+		map.put(9, "abhi");
+		map.put(6, "cahndru");
+		map.put(8, "sun");
+		map.put(10, "savita");
+
+		System.out.println(map);
+
+	}
+
+}
