4e760d638bff53b5c7d6983f5fb437e0ba6c70cd
Rahul Rajput <101698795+Rahul170699@users.noreply.github.com>
2022-04-04 23:37:30 +0530

    Add files via upload

---
+++ Rahul Rajput/Assignment-08/src/test/java/com/example/demo/BookSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
